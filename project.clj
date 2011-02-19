3a472ecdfe77589b2a15b1dd373abc64ad75d4a5
dj <dy10@hampshire.edu>
2011-02-19 15:36:57 -0500

    Added lein-related things.
    ADD: project.clj
    ADD: clojure and related dependencies in jar

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject autoevo "1.0.0"
+  :description "Autoconstructive evolutionary methods"
+  :url "http://github.com/dj/autoevo"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
