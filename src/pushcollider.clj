8fc6e0d365917468d2776d5dec2f61ff20681979
Killeroid <ky10@hampshire.edu>
2011-05-11 17:26:41 -0400

    Blah. Another day another null pointer exception

--- src/pushcollider.clj
+++ src/pushcollider.clj
@@ -55,13 +55,14 @@
                           'autocrossover1
                           'autocrossover2))
 
+
 (defn new-individual
   "Returns a new, evaluated individual. "
   [& {:keys [genome error totalerror ancestor history]
       :or {genome (random-code max-points pushcollider-atom-generators)
            error 0
            totalerror 0
-           ancestor ()
+           ancestor nil
            history 0}}]
   (let [err (evaluate genome)
         totalerr (+ totalerror err)]
@@ -69,16 +70,21 @@
 
 
   
+(defn after-eval
+  "Returns top item on type stack in state after pushing individual unto
+code stack and then exec'ing it"
+  ([individual stack] (after-eval individual stack (make-push-state)))
+  ([individual stack state]
+    (top-item stack 
+      (run-push individual
+        (push-item 0 :auxiliary 
+          (push-item individual :code state))))))
+  
+
+
 (defn autoconstruct
-  "Autoconstruct by pushing item on stack and then running item on a copy
-itself already on the stack"
-  ([pgm] autoconstruct pgm make-push-state)
-  ([pgm state]
-    (top-item :code 
-    (run-push pgm 
-      (push-item 0 :auxiliary 
-        (push-item pgm :code
-          (state)))))))
+  [pgm]
+  (after-eval pgm :code))
 
 (defn stagnant
   "Checks history of individual to make sure individual isnt just being recycled"
@@ -105,58 +111,16 @@ itself already on the stack"
     (when-let [fun (ns-resolve *ns* (symbol nm))]
         (apply fun args)))
 
-(defn stringtokenizer
-  "Breaks strings into small pieces"
-  [string end]
-  (let [abortion (string/split #" " (str string))
-        yeah (map (fn [x] (symbol x)) (rest abortion))]
-    (if (= end 1)
-      (str (first abortion))
-      (not-lazy (vector yeah)))))
-
-(defn babymaker
-"Making babies"
-[state [i1 i2]]
-(let [x (atom 0)]
-(push-item (top-item :integer (run-push (:genome i1) (push-item 0 :auxiliary (push-item (:genome i1) :code state)))) :hay state)
-(push-item (top-item :integer (run-push (:genome i2) (push-item 0 :auxiliary (push-item (:genome i2) :code state)))) :hay state)
+(defn arrity
+  "Find arrity of operators"
+  [s]
+  (count (first (:arglists (meta (resolve (symbol s)))))))
 
 
-(while (< @x (* (count mutation-operators) 2))
-                        (push-item (top-item :integer 
-                          (run-push (:genome 
-                                      (call 
-                                        (symbol (str (nth mutation-operators @x)))
-                                        (if (= (count (first (:arglists (meta (resolve (symbol (str (nth mutation-operators @x)))))))) 1)
-                                          (if (= (mod @x 2) 0)
-                                            i1
-                                            i2)
-                                          (if (= (mod @x 2) 0)
-                                            (i1 i2)
-                                            (i2 i1))))
-                            (push-item @x :auxiliary
-                              (push-item (:genome  
-                                           (call 
-                                             (symbol (str (nth mutation-operators @x)))
-                                             (if (= (count (first (:arglists (meta (resolve (symbol (str (nth mutation-operators @x)))))))) 1)
-                                               (if (= (mod @x 2) 0)
-                                                 i1
-                                                 i2)
-                                               (if (= (mod @x 2) 0)
-                                                 (i1 i2)
-                                                 (i2 i1))))) 
-                                (:code (state))))))) :hay state)
-(if (= (top-item :hay state) :no-stack-item)
-  ((pop-item :hay state) (push-item @x :hay state)))
-(swap! x inc))
-(if (= (mod (count (:hay state)) 2) 1)
-  (push-item @x :hay state))
-  
-(concat (:hay state))))
 
 (defn mutate
   "Returns an evaluated individual resulting from the autoconstructive mutation of i."
-  [i]
+  [[i]]
   (let [old-genome (:genome i)
         randomx (random-code max-points pushcollider-atom-generators)
         descendant (autoconstruct (:genome i))
@@ -183,7 +147,7 @@ itself already on the stack"
 (defn crossover
   "Returns an evaluated individual resulting from the recombination 
 of i1 and i2."
-  [i1 i2]
+  [[i1 i2]]
   (let [new-genome (insert-code-at-point (:genome i1) 
                      (select-node-index (:genome i1))
                      (code-at-point (:genome i2)
@@ -200,7 +164,7 @@ of i1 and i2."
   "Returns an evaluated individual resulting from the autoconstruction of
 one subtree and the subsequent translocation of that subtree to replace
 another subtree in the same tree."
-  [i]
+  [[i]]
   (let [old-genome (:genome i)
         place1 (select-node-index old-genome)
         place2 (select-node-index old-genome)
@@ -227,7 +191,7 @@ another subtree in the same tree."
 (defn autocrossover1
   "Same as the mutation operator autocrossover except both subtrees are autoconstructed
 and both subtrees are replaced"
-  [i]
+  [[i]]
   (let [old-genome (:genome i)
         place1 (select-node-index old-genome)
         place2 (select-node-index old-genome)
@@ -261,7 +225,7 @@ and both subtrees are replaced"
 (defn autocrossover2
   "Same as the mutation operator autocrossover except subtrees are on
 two different trees"
-  [i1 i2]
+  [[i1 i2]]
   (let [old-genome (:genome i1)
         old-genome2 (:genome i2)
         place1 (select-node-index old-genome)
@@ -290,7 +254,7 @@ two different trees"
 
 (defn autopmutate
   "Returns an evaluated individual resulting from the autoconstructive point mutation of i."
-  [i]
+  [[i]]
   (let [old-genome (:genome i)
         place (select-node-index old-genome)
         stree (code-at-point old-genome place)
@@ -310,59 +274,74 @@ two different trees"
         :genome new-genome
         :ancestor ancestor))))
 
+  
 
-(defn constructive-collision
+(defn constructive-collision 
   "Takes a pair of individuals and returns a vector of individuals."
   [[i1 i2]]
-  (let [state (make-push-state)
-        probability (babymaker state [[i1 i2]])
-        probs (map (fn [x] x) (:hay state))
-        x (atom 0)
-        y (atom 0)]
-(assoc state :hay '())
-(push-item (top-item :code (run-push (:genome i1) (push-item 0 :auxiliary (push-item (:genome i1) :code (state))))) :hay state)
-(push-item (top-item :code (run-push (:genome i2) (push-item 0 :auxiliary (push-item (:genome i2) :code (state))))) :hay state)
+  (def mutation-operator (vector
+                          i1
+                          i2
+                          ('mutate i1)
+                          ('mutate i2)
+                          ('crossover i1 i2)
+                          ('crossover i2 i1)
+                          ('autopmutate i1)
+                          ('autopmutate i2)
+                          ('autocrossover i1)
+                          ('autocrossover i2)
+                          ('autocrossover1 i1)
+                          ('autocrossover1 i2)
+                          ('autocrossover2 i1 i2)
+                          ('autocrossover2 i2 i1)))
+  (let [times (mod (:error (rand-nth [i1 i2])) 10)
+        state (make-push-state)
+        ;pos (atom 0)
+        ;probs (map (fn [x] (mod (:error x) 10)) mutation-operator)
+        ]
+;;Loops number of mutation-operators
+;(loop [x 0]
+;(when (< x (count mutation-operator)) 
+  ;;Insert (loop 2)
+;;
+;(loop [y 0
+;       indivs (vector i1 i2)]
+;(when (< y (count mutation-operator))
+
+;;Loops number of times each mutation-operator instance is run 
+;(loop [pos 0]
+;]
+;    (if (< pos times)
+;      results      
+;      (recur (inc pos) (nth prob x) (into (nth mutation-operator x) res)))
+;)
+
+;(recur (inc x))))
+;(print-return (vec (:code state)))
+;(print-return (mapcat (fn [x] (nth mutation-operator x))))
+;(apply (fn [x])
+;(vec (:code state))
+;)
+;;Loops for number of times for a mutator
+;(print-return probs)
+(def foo
+  (fn [y pos res]
+  (if (< pos 0)
+    res
+    #(foo y (dec pos) (conj res (nth mutation-operator y))))))
+
+(def foo1
+  (fn [x results]
+  (if (= (+ x 1) (count mutation-operator))
+    results
+    #(foo1 (inc x) (vec (concat results (trampoline foo (inc x) (int (* (lrand) 10)) results)))))))
+
+(trampoline foo1 -1 (vector))
+))
 
 
-(while (< @x (* (count mutation-operators) 2)) 
-(while (<= @y (nth probs @x))
-                        (push-item (top-item :code
-                          (run-push (:genome 
-                                      (call 
-                                        (symbol (str (nth mutation-operators @x)))
-                                        (if (= (count (first (:arglists (meta (resolve (symbol (str (nth mutation-operators @x)))))))) 1)
-                                          (if (= (mod @y 2) 0)
-                                            i1
-                                            i2)
-                                          (if (= (mod @y 2) 0)
-                                            (i1 i2)
-                                            (i2 i1))))
-                            (push-item @y :auxiliary
-                              (push-item (:genome  
-                                           (call 
-                                             (symbol (str (nth mutation-operators @x)))
-                                             (if (= (count (first (:arglists (meta (resolve (symbol (str (nth mutation-operators @x)))))))) 1)
-                                               (if (= (mod @y 2) 0)
-                                                 i1
-                                                 i2)
-                                               (if (= (mod @y 2) 0)
-                                                 (i1 i2)
-                                                 (i2 i1))))) 
-                                (:code (state))))))) :hay state)
-(swap! y inc))
-(swap! x inc))
- 
-(vec (:hay state))))
-        
- 
-(comment
-    (for [x (range (count mutation-operators))]
-      (push-item (call 
-                   (stringtokenizer (nth mutation-operators x) 1) 
-                   (stringtokenizer (nth mutation-operators x) 2)) :auxiliary state))
-    (if (not= (mod (reduce + (remove #{:no-stack-item} babymaker)) 2))
-          (push-item i1 :auxiliary state))
-    (concat (take (reduce + (remove #{:no-stack-item} babymaker)) (:auxiliary state))))
+  
+
     
 
 (defn constructive-collisions
@@ -413,5 +392,8 @@ the population."
                            (constructive-collisions population))
                          base-pop-size))]
         (recur (inc generation) next-gen)))))
+
+
+  
       
   
