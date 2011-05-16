d6b862c589e4664416cb264967e2ca6db5910f53
Killeroid <ky10@hampshire.edu>
2011-05-15 23:31:47 -0400

    Merged all experimental branches. Added Tim's mutators

--- src/pushcollider.clj
+++ src/pushcollider.clj
@@ -1,5 +1,5 @@
 (ns pushcollider
-  (:require [clojush :exclude (mutate crossover report)] [clojure.contrib.math] [clojure.contrib.string :as string])
+  (:require [clojush :exclude (mutate crossover report)] [clojure.contrib.math] [clojure.contrib.string :as string] [clojure.zip :as zip])
   (:use [clojush :exclude (mutate crossover report)] [clojure.contrib.math]))
 
 (def succeeded (atom false)) ;; global flag to indicate success
@@ -16,7 +16,7 @@
 (def max-points 50) ;;max points any program can have
 (def loopy 3)
 
-(defrecord ind [genome error totalerror ancestor history]) ;; data structure for individuals
+(defrecord ind [genome error totalerror ancestor history oper]) ;; data structure for individuals
 
 
 (defn report-success
@@ -55,15 +55,16 @@
 
 (defn new-individual
   "Returns a new, evaluated individual. "
-  [& {:keys [genome error totalerror ancestor history]
+  [& {:keys [genome error totalerror ancestor history oper]
       :or {genome (random-code max-points pushcollider-atom-generators)
            error 0
            totalerror 0
            ancestor nil
-           history 0}}]
+           history 0
+           oper ""}}]
   (let [err (evaluate genome)
         totalerr (+ totalerror err)]
-  (ind. genome err totalerr ancestor history)))
+  (ind. genome err totalerr ancestor history oper)))
 
 
   
@@ -94,7 +95,8 @@ itself already on the stack"
               :error (get i :error()) 
               :totalerror (get i :totalerror()) 
               :ancestor (get i :ancestor())
-              :history (inc (get i :history())))
+              :history (inc (get i :history()))
+              :oper (get i :oper()))
             (new-individual
               :genome (random-code max-points pushcollider-atom-generators))))
 
@@ -113,10 +115,10 @@ itself already on the stack"
 ;Calls function recursively till condition is fulfilled
 ;Used for generating random # of baies from each operator
 (def caller
-  (fn [op args y results]
+  (fn [y results op & args]
     (if (= y 0)
       results
-      #(caller op args (dec y) (conj results (apply op args)))))) 
+      #(caller (dec y) (conj results (apply op args)) op args)))) 
 
 (defn arrity
   "Find arrity of operators"
@@ -125,7 +127,7 @@ itself already on the stack"
 
 (defn mutate
   "Returns an evaluated individual resulting from the autoconstructive mutation of i."
-  [[i]]
+  [i]
   (let [old-genome (:genome i)
         randomx (random-code max-points pushcollider-atom-generators)
         descendant (autoconstruct (:genome i))
@@ -141,19 +143,20 @@ itself already on the stack"
                  (= (discrepancy old-genome descendant) 0)
                  (some #{descendant} ancestor)
                  )]
-    (swap! op1 inc) ;;Increment each time operator is called
+    ;(swap! op1 inc) ;;Increment each time operator is called
     (if failed
       (new-individual :genome randomx)  
       (new-individual
         :genome descendant
         :error child-error
-        :ancestor ancestor))))
+        :ancestor ancestor
+        :oper "mutate"))))
        
 
 (defn crossover
   "Returns an evaluated individual resulting from the recombination 
 of i1 and i2."
-  [[i1 i2]]
+  [i1 i2]
   (let [new-genome (insert-code-at-point (:genome i1) 
                      (select-node-index (:genome i1))
                      (code-at-point (:genome i2)
@@ -165,13 +168,14 @@ of i1 and i2."
       (stagnant i1)
       (new-individual
         :genome new-genome 
-        :ancestor ancestor))))
+        :ancestor ancestor
+        :oper "crossover"))))
 
 (defn autocrossover
   "Returns an evaluated individual resulting from the autoconstruction of
 one subtree and the subsequent translocation of that subtree to replace
 another subtree in the same tree."
-  [[i]]
+  [i]
   (let [old-genome (:genome i)
         place1 (select-node-index old-genome)
         place2 (select-node-index old-genome)
@@ -194,12 +198,14 @@ another subtree in the same tree."
       (stagnant i)
       (new-individual
         :genome new-genome
-        :ancestor ancestor))))
+        :ancestor ancestor
+        :oper "autocrossover"
+        ))))
 
 (defn autocrossover1
   "Same as the mutation operator autocrossover except both subtrees are autoconstructed
 and both subtrees are replaced"
-  [[i]]
+  [i]
   (let [old-genome (:genome i)
         place1 (select-node-index old-genome)
         place2 (select-node-index old-genome)
@@ -229,12 +235,13 @@ and both subtrees are replaced"
       (stagnant i)
       (new-individual
         :genome new-genome
-        :ancestor ancestor))))
+        :ancestor ancestor
+        :oper "autocrossover1"))))
 
 (defn autocrossover2
   "Same as the mutation operator autocrossover except subtrees are on
 two different trees"
-  [[i1 i2]]
+  [i1 i2]
   (let [old-genome (:genome i1)
         old-genome2 (:genome i2)
         place1 (select-node-index old-genome)
@@ -258,12 +265,13 @@ two different trees"
       (stagnant i1)
       (new-individual
         :genome new-genome
-        :ancestor ancestor))))
+        :ancestor ancestor
+        :oper "autocrossover2"))))
 
 
 (defn autopmutate
   "Returns an evaluated individual resulting from the autoconstructive point mutation of i."
-  [[i]]
+  [i]
   (let [old-genome (:genome i)
         place (select-node-index old-genome)
         stree (code-at-point old-genome place)
@@ -282,9 +290,55 @@ two different trees"
       (stagnant i)
       (new-individual
         :genome new-genome
-        :ancestor ancestor))))
+        :ancestor ancestor
+        :oper "autopmutate"))))
+
+;;Tim had an identical mutator
+(defn sizeFairMutation
+  "Returns an evaluated individual resulting from the replacement of a subtree
+with a randomly generated subtree with same number of points."
+  [i]
+  (let [old-genome (:genome i)
+        place (select-node-index old-genome)
+        stree (random-code-with-size (count-points place) pushcollider-atom-generators)
+        new-genome (insert-code-at-point old-genome place stree)
+        ancestor (make-ancestor (get i :ancestor ()) old-genome)]
+    (if (> (count-points new-genome) max-points)
+      (stagnant i)
+      (new-individual
+        :genome new-genome
+        :ancestor ancestor
+        :oper "sizeFairMutation"))))
+
+;;Contributed by Tim
+(defn mutateAll2
+  "Mutates Acros the board wth p % of every point being replaced by a subtree"
+  [i p]
+  (let [old-genome (:genome i)
+        zipper (zip/seq-zip old-genome)
+        new-genome (loop [z zipper j 0]
+                     (if (>= j (count-points old-genome))
+                       (zip/root z)
+                       (recur
+                         (if-not (zip/end? z)
+                           (if-not (seq? (zip/node z))
+                             (if (< (rand-int 100) p)
+                               (zip/next (zip/replace z (random-code 1
+                                                          pushcollider-atom-generators)))
+                               (zip/next z))
+                             (zip/next z))
+                           z)
+                         (inc j))))
+        ancestor (make-ancestor (get i :ancestor ()) old-genome)]
+    (if (> (count-points new-genome) max-points)
+      (stagnant i)
+      (new-individual
+        :genome new-genome
+        :ancestor ancestor
+        :oper "mutateAll2"))))
 
 
+(comment
 (defn constructive-collision 
   "Takes a pair of individuals and returns a vector of individuals."
   [[i1 i2]]
@@ -293,7 +347,20 @@ two different trees"
     (caller crossover [i1 i2] (lrand-int 10) []) (caller crossover [i2 i1] (lrand-int 10) [])
     (caller autocrossover [i1] (lrand-int 10) []) (caller autocrossover [i2] (lrand-int 10) [])
     (caller autocrossover1 [i1] (lrand-int 10) []) (caller autocrossover1 [i2] (lrand-int 10) [])
-    (caller autocrossover2 [i1 i2] (lrand-int 10) []) (caller autocrossover2 [i2 i1] (lrand-int 10) [])))))
+    (caller autocrossover2 [i1 i2] (lrand-int 10) []) (caller autocrossover2 [i2 i1] (lrand-int 10) []))))))
+
+(defn constructive-collision
+  "Takes a pair of individuals and returns a vector of individuals."
+  [[i1 i2]]
+  (vector i1 i2
+    (mutate i1) (mutate i2)
+    (crossover i1 i2) (crossover i2 i1)
+    (autocrossover i1) (autocrossover i2)
+    (autocrossover1 i1) (autocrossover1 i2)
+    (autocrossover2 i1 i2) (autocrossover2 i2 i1)
+    (autopmutate i1) (autopmutate i2)
+    (mutateAll2 i1 (lrand-int 100)) (mutateAll2 i2 (lrand-int 100))
+    (sizeFairMutation i1) (sizeFairMutation i2)))
 
 (defn constructive-collisions
   "Splits the population into pairs, collides the pairs constructively
@@ -315,7 +382,7 @@ and returns a vector of all of the resulting individuals."
   (if (< (count population) base-pop-size)
     population
     (recur 
-      (map destructive-collision 
+      (pmap destructive-collision 
         (partition 2 2 population population))
       base-pop-size)))
 
@@ -327,7 +394,8 @@ the population."
                  population)]
   (println "Generation: " generation 
     ", lowest error: " (apply min (map :error population))
-    ", Best Program: " (:genome best)))
+    ", Best Program: " (:genome best)
+    ", Generated by: " (:oper best)))
   population)
 
 (defn evolve
